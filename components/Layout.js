dcd0f796e9675366288afa7eb8231c9e5fb41d42
DiffusedPizza <88430429+DiffusedPizza@users.noreply.github.com>
2024-03-12 17:58:36 -0700

    corrected template syntax

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,15 @@
 //FONTS
 import {Sora} from '@next/font/google'
 
-
 //Font settings
 const sora = Sora({
   subsets: ['latin'],
   variable: '--font-sora',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 });
+
+//COMPONENTS
+import Nav from '../components/Nav';
 
 
 const Layout = ({children}) => {
