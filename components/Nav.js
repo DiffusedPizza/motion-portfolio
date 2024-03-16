1be52d6088cdf4de79c817f61564717e56a99583
DiffusedPizza <rubenlizardi79@gmail.com>
2024-03-16 08:28:22 -0700

    edited nav

--- components/Nav.js
+++ components/Nav.js
@@ -26,14 +26,23 @@ export const navData = [
   },
 ];
 
-// Next link
+//next link
 import Link from 'next/link';
 
-// Next router
-import { useRouter} from 'next/router';
+//next router
+import { useRouter } from 'next/router';
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return <nav>
+    {/* inner */}
+    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-red-200/10'>
+      {navData.map((link, index)=> {
+        return <Link href={link.path}>{link.icon}</Link>
+      })}
+    </div>
+  </nav>;
 };
 
 export default Nav;
